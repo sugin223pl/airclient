e98286ab25dd816138fa209d5970d255dbbf80e0
Sugin Paladin <34408293+sugin223pl@users.noreply.github.com>
2022-03-25 01:01:24 +0200

    Added files

---
+++ precache-manifest.5328fc098330c4a6d3350716e1f2895a.js
@@ -0,0 +1,230 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "992d6bc9ef1e9309a8c4e8ddc133c7c4",
+    "url": "/assets/invoice/receiptHtml.html"
+  },
+  {
+    "revision": "f93151814f82353e3ae52b016dc11bef",
+    "url": "/assets/invoice/receiptTxt.txt"
+  },
+  {
+    "revision": "bf71b082939a954753e70bd2bf74bd5a",
+    "url": "/assets/invoice/requestHtml.html"
+  },
+  {
+    "revision": "0e5d3406416ca0a8f4cb7c62cab9e4e7",
+    "url": "/assets/invoice/sms.txt"
+  },
+  {
+    "revision": "df0eeab52309cfa37e2f",
+    "url": "/css/5280e027be02.a7fd9b93.css"
+  },
+  {
+    "revision": "cb3321938d2581e95389",
+    "url": "/css/8765e027b765.f0ae3dc5.css"
+  },
+  {
+    "revision": "8ac3b78ab840e61b0741",
+    "url": "/css/app.e3be24e5.css"
+  },
+  {
+    "revision": "dc7099561fc87f94fbd4",
+    "url": "/css/b09e3f45454e.3925ccaf.css"
+  },
+  {
+    "revision": "6ba7950f434d87c927ae",
+    "url": "/css/b09e3f45454e~p2g53f45330e.5192cece.css"
+  },
+  {
+    "revision": "04c23e43541dfb2a234d",
+    "url": "/css/chunk-08c24c81.9e82057d.css"
+  },
+  {
+    "revision": "a61354d451474d7e7117",
+    "url": "/css/chunk-6eb3430d.51e8954b.css"
+  },
+  {
+    "revision": "6ee27cbd574f0087bf97",
+    "url": "/css/p2g53f45330e.523fd801.css"
+  },
+  {
+    "revision": "33517106fc325ce3c5507bbf8830ec0f",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "21a97ec4480654af9d352a0919302610",
+    "url": "/img/android/android-launchericon-144-144.png"
+  },
+  {
+    "revision": "bd5aa33d0d9cd6e1a6e74de3ada45e18",
+    "url": "/img/android/android-launchericon-192-192.png"
+  },
+  {
+    "revision": "0c19665bb7a21b29366ad8e4cd7ce156",
+    "url": "/img/android/android-launchericon-48-48.png"
+  },
+  {
+    "revision": "1ff7a8342c36a8a3e18899a80c4070ea",
+    "url": "/img/android/android-launchericon-512-512.png"
+  },
+  {
+    "revision": "1f2a95b1f355e2748290fd0fefa1a67d",
+    "url": "/img/android/android-launchericon-72-72.png"
+  },
+  {
+    "revision": "494b5e387791a6dc106cfd515aa03246",
+    "url": "/img/android/android-launchericon-96-96.png"
+  },
+  {
+    "revision": "0c19665bb7a21b29366ad8e4cd7ce156",
+    "url": "/img/chrome/chrome-extensionmanagementpage-48-48.png"
+  },
+  {
+    "revision": "f9ef2cf375304cfe4d83596fa2030b1c",
+    "url": "/img/chrome/chrome-favicon-16-16.png"
+  },
+  {
+    "revision": "7052deabb05b5dc28aac445bd041c5a9",
+    "url": "/img/chrome/chrome-installprocess-128-128.png"
+  },
+  {
+    "revision": "7052deabb05b5dc28aac445bd041c5a9",
+    "url": "/img/firefox/firefox-general-128-128.png"
+  },
+  {
+    "revision": "f9ef2cf375304cfe4d83596fa2030b1c",
+    "url": "/img/firefox/firefox-general-16-16.png"
+  },
+  {
+    "revision": "4517fa5480348d201b31e6455c95d5a7",
+    "url": "/img/firefox/firefox-general-256-256.png"
+  },
+  {
+    "revision": "e80ba87e24f43a0cd75b6544dc5f8330",
+    "url": "/img/firefox/firefox-general-32-32.png"
+  },
+  {
+    "revision": "0c19665bb7a21b29366ad8e4cd7ce156",
+    "url": "/img/firefox/firefox-general-48-48.png"
+  },
+  {
+    "revision": "deb01de3c55374e8ee5ab6d7cd3f0d65",
+    "url": "/img/firefox/firefox-general-64-64.png"
+  },
+  {
+    "revision": "44a22f5435f804fefa19dbad0146e93a",
+    "url": "/img/firefox/firefox-general-90-90.png"
+  },
+  {
+    "revision": "7052deabb05b5dc28aac445bd041c5a9",
+    "url": "/img/firefox/firefox-marketplace-128-128.png"
+  },
+  {
+    "revision": "1ff7a8342c36a8a3e18899a80c4070ea",
+    "url": "/img/firefox/firefox-marketplace-512-512.png"
+  },
+  {
+    "revision": "fbe7368b29c0b188139bdd8c97a5c407",
+    "url": "/img/icons/38905c3d4a96.svg"
+  },
+  {
+    "revision": "6410bc2c1059d1554342561b65864dd0",
+    "url": "/img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "31ed5c4060dc0321ccfe3aa76e120a37",
+    "url": "/img/icons/android-icon-192x192.png"
+  },
+  {
+    "revision": "52765510951a4f225ee219bdb4d13b36",
+    "url": "/img/icons/android-icon-512x512.png"
+  },
+  {
+    "revision": "2e99848d7d20b10197e2021eeaf9aa9d",
+    "url": "/img/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "3006814f5067c3d0a54451bd000d7e40",
+    "url": "/img/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "af5c54b69b561ac16aa287ae200aa5fc",
+    "url": "/img/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "537adb4d59a18cd6c153c825b5eceeac",
+    "url": "/img/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "4203fa8a02135ba62c090980fe729be4",
+    "url": "/img/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "341c15e60b0fc236770ae6bde7ed7683",
+    "url": "/img/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "33517106fc325ce3c5507bbf8830ec0f",
+    "url": "/img/icons/favicon.ico"
+  },
+  {
+    "revision": "da0181a562d3300038b4dbfb9d7b566b",
+    "url": "/img/icons/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "fbe7368b29c0b188139bdd8c97a5c407",
+    "url": "/img/icons/safari-pinned-tab.svg.svg"
+  },
+  {
+    "revision": "4c35e984ed86fbc5d6674cbfcd4b64c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df0eeab52309cfa37e2f",
+    "url": "/js/5280e027be02.0346999e.js"
+  },
+  {
+    "revision": "cb3321938d2581e95389",
+    "url": "/js/8765e027b765.3e9be903.js"
+  },
+  {
+    "revision": "8ac3b78ab840e61b0741",
+    "url": "/js/app.540bdc9b.js"
+  },
+  {
+    "revision": "dc7099561fc87f94fbd4",
+    "url": "/js/b09e3f45454e.b37277af.js"
+  },
+  {
+    "revision": "6ba7950f434d87c927ae",
+    "url": "/js/b09e3f45454e~p2g53f45330e.f235f240.js"
+  },
+  {
+    "revision": "04c23e43541dfb2a234d",
+    "url": "/js/chunk-08c24c81.daf671f9.js"
+  },
+  {
+    "revision": "fbf91bc276245ab7d030",
+    "url": "/js/chunk-3ee50c52.4165e4ce.js"
+  },
+  {
+    "revision": "a61354d451474d7e7117",
+    "url": "/js/chunk-6eb3430d.1dc39aa5.js"
+  },
+  {
+    "revision": "1299d289e3831323288a",
+    "url": "/js/chunk-vendors.4db28af5.js"
+  },
+  {
+    "revision": "6ee27cbd574f0087bf97",
+    "url": "/js/p2g53f45330e.8a53775c.js"
+  },
+  {
+    "revision": "6fb5a47a0e7fa705f0b35940d8c35699",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
